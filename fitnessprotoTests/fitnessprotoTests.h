3c6ba4c971f44478dc58981e18a9f4135f9b2bbf
Bharath Sridharan <bharath28@gmail.com>
2011-07-01 10:55:06 -0500

    Adding Project files for the first time

---
+++ fitnessprotoTests/fitnessprotoTests.h
@@ -0,0 +1,17 @@
+//
+//  fitnessprotoTests.h
+//  fitnessprotoTests
+//
+//  Created by Sridharan, Bharath on 6/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface fitnessprotoTests : SenTestCase {
+@private
+    
+}
+
+@end
